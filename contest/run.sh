1fab23c5a31c5c61885a31044a61857d0595a70f
zhangzhexin <nonstopfor@gmail.com>
2021-05-23 21:15:52 +0800

    temp

--- contest/run.sh
+++ contest/run.sh
@@ -1,3 +1,3 @@
 echo Using GPU: [$1]
 
-CUDA_VISIBLE_DEVICES=$1 python3 run_attacks.py --attacks attacker --output tmp
+CUDA_VISIBLE_DEVICES=$1 python3 run_attacks.py --attacks attacker --output tmp --models cifar10-pgd_at
