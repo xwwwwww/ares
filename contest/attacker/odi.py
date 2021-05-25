d4ab3df6e4a8dd21e2bb1d495b7031f5718233ee
zhangzhexin <nonstopfor@gmail.com>
2021-05-25 16:51:16 +0800

    update odi

--- contest/attacker/odi.py
+++ contest/attacker/odi.py
@@ -6,27 +6,30 @@ from ares.attack.utils import maybe_to_array, uniform_l_2_noise, uniform_l_inf_n
 from ares.loss import CrossEntropyLoss
 
 
-class PGDAttacker(BatchAttack):
+class ODIPGDAttacker(BatchAttack):
     def __init__(self, model, batch_size, dataset, session):
         ''' Based on ares.attack.bim.BIM '''
-        self.name = 'pgd'
+        self.name = 'odi-pgd'
         self.model, self.batch_size, self._session = model, batch_size, session
         # dataset == "imagenet" or "cifar10"
-        loss = CrossEntropyLoss(self.model) # 定义loss
-        
-        
+        loss = CrossEntropyLoss(self.model)  # 定义loss
+
         # random init magnitude
         self.rand_init_eps_ph = tf.placeholder(self.model.x_dtype, (self.batch_size,))
         self.rand_init_eps_var = tf.Variable(tf.zeros((self.batch_size,), dtype=self.model.x_dtype))
-        
+
         # calculate init point within rand_init_eps
         d = np.prod(self.model.x_shape)
         noise = uniform_l_inf_noise(batch_size, d, self.rand_init_eps_var, self.model.x_dtype)
-        
+
+        # wd
+        output_dim = 10 if dataset == 'cifar10' else 1000
+        wd = uniform_l_inf_noise(batch_size, output_dim, 1., self.model.x_dtype)
+
         # placeholder for batch_attack's input
         self.xs_ph = get_xs_ph(model, batch_size)
         self.ys_ph = get_ys_ph(model, batch_size)
-        
+
         # clip by (x_min, x_max)
         xs_init = tf.clip_by_value(tf.reshape(self.xs_ph, (self.batch_size, -1)) + noise,
                                    self.model.x_min, self.model.x_max)
@@ -52,23 +55,23 @@ class PGDAttacker(BatchAttack):
         # calculate loss' gradient with relate to the adversarial example
         # grad.shape == (batch_size, D)
         self.xs_adv_model = tf.reshape(self.xs_adv_var, (batch_size, *self.model.x_shape))
-        self.loss = loss(self.xs_adv_model, self.ys_var) # 计算loss
-        grad = tf.gradients(self.loss, self.xs_adv_var)[0] # 得到对xs_adv的梯度
+        self.loss = loss(self.xs_adv_model, self.ys_var)  # 计算loss
+        grad = tf.gradients(self.loss, self.xs_adv_var)[0]  # 得到对xs_adv的梯度
         # update the adversarial example
         xs_lo, xs_hi = self.xs_var - eps, self.xs_var + eps
         grad_sign = tf.sign(grad)
         # clip by max l_inf magnitude of adversarial noise
-        xs_adv_next = tf.clip_by_value(self.xs_adv_var + alpha * grad_sign, xs_lo, xs_hi) # 计算新值
+        xs_adv_next = tf.clip_by_value(self.xs_adv_var + alpha * grad_sign, xs_lo, xs_hi)  # 计算新值
         # clip by (x_min, x_max)
         xs_adv_next = tf.clip_by_value(xs_adv_next, self.model.x_min, self.model.x_max)
-        
+
         # 初始化
 
         # clip by (x_min, x_max)
         xs_init = tf.clip_by_value(tf.reshape(self.xs_ph, (self.batch_size, -1)) + noise,
                                    self.model.x_min, self.model.x_max)
-        
-        self.update_xs_adv_step = self.xs_adv_var.assign(xs_adv_next) # 用计算出的新值更新
+
+        self.update_xs_adv_step = self.xs_adv_var.assign(xs_adv_next)  # 用计算出的新值更新
         self.config_eps_step = self.eps_var.assign(self.eps_ph)
         self.config_alpha_step = self.alpha_var.assign(self.alpha_ph)
         self.config_rand_init_eps = self.rand_init_eps_var.assign(self.rand_init_eps_ph)
@@ -82,12 +85,13 @@ class PGDAttacker(BatchAttack):
         if 'magnitude' in kwargs:
             self.eps = kwargs['magnitude'] - 1e-6
             eps = maybe_to_array(self.eps, self.batch_size)
-            self._session.run(self.config_eps_step, feed_dict={self.eps_ph: eps}) # 初始化
+            self._session.run(self.config_eps_step, feed_dict={self.eps_ph: eps})  # 初始化
             self._session.run(self.config_alpha_step, feed_dict={self.alpha_ph: eps / 7})
 
         # rand_init_magnitude = (1.0 / 255) * (self.model.x_max - self.model.x_min)
-        # rand_init_eps = maybe_to_array(rand_init_magnitude, self.batch_size)
-        # self._session.run(self.config_rand_init_eps, feed_dict={self.rand_init_eps_ph: rand_init_eps})
+        rand_init_magnitude = kwargs['magnitude'] - 1e-6
+        rand_init_eps = maybe_to_array(rand_init_magnitude, self.batch_size)
+        self._session.run(self.config_rand_init_eps, feed_dict={self.rand_init_eps_ph: rand_init_eps})
 
     def batch_attack(self, xs, ys=None, ys_target=None):
         num = 3
@@ -96,11 +100,11 @@ class PGDAttacker(BatchAttack):
         best_idx = None
 
         for i in range(num):
-            self._session.run(self.setup_xs, feed_dict={self.xs_ph: xs}) # 初始化 
+            self._session.run(self.setup_xs, feed_dict={self.xs_ph: xs})  # 初始化
             self._session.run(self.setup_ys, feed_dict={self.ys_ph: ys})
-            for _ in range(self.iteration): # 迭代K步
+            for _ in range(self.iteration):  # 迭代K步
                 self._session.run(self.update_xs_adv_step)
-            res.append(self._session.run(self.xs_adv_model)) # 返回结果
+            res.append(self._session.run(self.xs_adv_model))  # 返回结果
             loss = self._session.run(self.loss).mean().item()
             print(loss)
             if loss > loss_max:
