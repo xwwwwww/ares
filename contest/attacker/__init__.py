0e2d9054624be916e56d4e29e34f2e454d7180f6
xwwwwww <wenjx_thu@163.com>
2021-05-25 18:35:09 +0800

    fix import

--- contest/attacker/__init__.py
+++ contest/attacker/__init__.py
@@ -1,3 +1,4 @@
 # from .bim import Attacker
 # from .pgd import PGDAttacker as Attacker
-from .bim import BIMAttacker as Attacker
+# from .bim import BIMAttacker as Attacker
+from .odi import ODIPGDAttacker as Attacker
